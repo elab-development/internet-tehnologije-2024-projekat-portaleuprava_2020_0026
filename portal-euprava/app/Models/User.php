a2c30d65cf6a77c021e4e8ab946dc5263427b112
Nebojsa Delic <nebodelaue@gmail.com>
2025-12-22 12:04:13 +0100

    Podesavanja autentifikacije implementirana

--- portal-euprava/app/Models/User.php
+++ portal-euprava/app/Models/User.php
@@ -6,9 +6,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     use HasFactory;
     use Notifiable;
 
