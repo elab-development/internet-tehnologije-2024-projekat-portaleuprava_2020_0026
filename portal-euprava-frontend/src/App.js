f5f0e0e9c2c02ba0e8b554f88db76ddb78bf4b79
Nebojša Delić <nebodelaue@gmail.com>
2025-12-29 15:12:30 +0000

    Sve uvezano u okviru korene komponente App.js

--- portal-euprava-frontend/src/App.js
+++ portal-euprava-frontend/src/App.js
@@ -6,8 +6,17 @@ import Auth from "./pages/Auth";
 import HomeCitizen from "./pages/HomeCitizen";
 import HomeOfficer from "./pages/HomeOfficer";
 import HomeAdmin from "./pages/HomeAdmin";
-import NewRequestCitizen from "./pages/NewRequestCitizen"; // ✅ DODATO
+import NewRequestCitizen from "./pages/NewRequestCitizen"; 
+import MyRequestsCitizen from "./pages/MyRequestsCitizen";
+import ServicesCitizen from "./pages/ServicesCitizen";
+import InboxOfficer from "./pages/InboxOfficer";
+import AssignedToMeRequestsOfficer from "./pages/AssignedToMeRequestsOfficer";
+import StatisticsOfficer from "./pages/StatisticsOfficer";
 import ProtectedRoute from "./components/ProtectedRoute";
+import StatisticsAdmin from "./pages/StatisticsAdmin";
+import InstitutionsAdmin from "./pages/InstitutionsAdmin";
+import ServicesAdmin from "./pages/ServicesAdmin";
+import UsersAdmin from "./pages/UsersAdmin";
 
 import { getRole, isLoggedIn } from "./utils/auth";
 
@@ -38,7 +47,7 @@ export default function App() {
         }
       />
 
-      {/* ✅ NOVI ZAHTEV (CITIZEN) */}
+      {/* NOVI ZAHTEV (CITIZEN) */}
       <Route
         path="/citizen/new-request"
         element={
@@ -47,6 +56,106 @@ export default function App() {
           </ProtectedRoute>
         }
       />
+
+      {/* MOJI ZAHTEVI (CITIZEN) */}
+      <Route
+        path="/citizen/requests"
+        element={
+          <ProtectedRoute roles={["CITIZEN"]}>
+            <MyRequestsCitizen />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* SERVISI (CITIZEN) */}
+      <Route
+        path="/citizen/services"
+        element={
+          <ProtectedRoute roles={["CITIZEN"]}>
+            <ServicesCitizen />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* INBOX (OFFICER) */}
+      <Route
+        path="/officer/inbox"
+        element={
+          <ProtectedRoute roles={["OFFICER"]}>
+            <InboxOfficer />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* DODELJENI ZAHTEVI (OFFICER) */}
+      <Route
+        path="/officer/assigned"
+        element={
+          <ProtectedRoute roles={["OFFICER"]}>
+            <AssignedToMeRequestsOfficer />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* STATISTIKA (OFFICER) */}
+      <Route
+        path="/officer/stats"
+        element={
+          <ProtectedRoute roles={["OFFICER"]}>
+            <StatisticsOfficer />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* STATISTIKA (ADMIN) */}
+      <Route
+        path="/admin/stats"
+        element={
+          <ProtectedRoute roles={["ADMIN"]}>
+            <StatisticsAdmin />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* STATISTIKA (ADMIN) */}
+      <Route
+        path="/admin/stats"
+        element={
+          <ProtectedRoute roles={["ADMIN"]}>
+            <StatisticsAdmin />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* INSTITUCIJE (ADMIN) */}
+      <Route
+        path="/admin/institutions"
+        element={
+          <ProtectedRoute roles={["ADMIN"]}>
+            <InstitutionsAdmin />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* SERVISI (ADMIN) */}
+      <Route
+        path="/admin/services"
+        element={
+          <ProtectedRoute roles={["ADMIN"]}>
+            <ServicesAdmin />
+          </ProtectedRoute>
+        }
+      />  
+
+      {/* KORISNICI (ADMIN) */}
+      <Route
+        path="/admin/users"
+        element={
+          <ProtectedRoute roles={["ADMIN"]}>
+            <UsersAdmin />
+          </ProtectedRoute>
+        }
+      />  
 
       <Route
         path="/home-officer"
