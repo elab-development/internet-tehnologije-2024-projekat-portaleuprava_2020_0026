3e2c3fe80744bf8f5052ecafd3064b9e29608471
Nebojša Delić <nebodelaue@gmail.com>
2025-12-29 15:12:30 +0000

    Ispravka autentifikacije na backendu

--- portal-euprava/app/Http/Controllers/AuthController.php
+++ portal-euprava/app/Http/Controllers/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $data = $request->validate([
-            'email' => 'required|email:rfc,dns',
+            'email' => 'required|email',
             'password' => 'required|string',
         ]);
 
